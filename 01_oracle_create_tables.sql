4e8dcd013ecc7a938084f8fa7b3c6459000cb510
defilippomattia <defilippomattia@gmail.com>
2022-10-24 18:27:23 +0200

    looks like final er

--- 01_oracle_create_tables.sql
+++ 01_oracle_create_tables.sql
@@ -6,15 +6,6 @@ CREATE TABLE system.grad
   PRIMARY KEY (grad_id)
 );
 
-CREATE TABLE system.kvart
-(
-  kvart_id INT NOT NULL,
-  ime VARCHAR(50) NOT NULL,
-  grad_id INT NOT NULL,
-  PRIMARY KEY (kvart_id),
-  FOREIGN KEY (grad_id) REFERENCES system.grad(grad_id)
-);
-
 CREATE TABLE system.sportski_objekt
 (
   sportski_objekt_id INT NOT NULL,
@@ -25,18 +16,6 @@ CREATE TABLE system.sportski_objekt
   FOREIGN KEY (grad_id) REFERENCES system.grad(grad_id)
 );
 
-CREATE TABLE system.teren
-(
-  teren_id INT NOT NULL,
-  vrsta_terena VARCHAR(50) NOT NULL,
-  natkriven VARCHAR(50) NOT NULL,
-  sportski_objekt_id INT NOT NULL,
-  kvart_id INT NOT NULL,
-  PRIMARY KEY (teren_id),
-  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
-  FOREIGN KEY (kvart_id) REFERENCES system.kvart(kvart_id) --dodano
-);
-
 CREATE TABLE system.korisnik
 (
   oib INT NOT NULL,
@@ -44,36 +23,6 @@ CREATE TABLE system.korisnik
   prezime VARCHAR(50) NOT NULL,
   PRIMARY KEY (oib)
 );
-
-
-
-CREATE TABLE system.rezervacija
-(
-  pocetak VARCHAR(50) NOT NULL,
-  kraj VARCHAR(50) NOT NULL,
-  rezervacija_id INT NOT NULL,
-  teren_id INT NOT NULL,
-  oib INT NOT NULL,
-  PRIMARY KEY (rezervacija_id),
-  FOREIGN KEY (teren_id) REFERENCES system.teren(teren_id),
-  FOREIGN KEY (oib) REFERENCES system.korisnik(oib)
-);
-
-
-
-CREATE TABLE system.rekvizit
-(
-  rekvizit_id INT NOT NULL,
-  tip VARCHAR(50) NOT NULL,
-  iznajmljen VARCHAR(50) NOT NULL,
-  sportski_objekt_id INT NOT NULL,
-  rezervacija_id INT NOT NULL,
-  PRIMARY KEY (rekvizit_id),
-  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
-  FOREIGN KEY (rezervacija_id) REFERENCES system.rezervacija(rezervacija_id)
-);
-
-
 
 CREATE TABLE system.dodatni_sadrzaj
 (
@@ -89,4 +38,50 @@ CREATE TABLE system.objekt_sadrzaj
   PRIMARY KEY (sportski_objekt_id, dodatni_sadrzaj_id),
   FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
   FOREIGN KEY (dodatni_sadrzaj_id) REFERENCES system.dodatni_sadrzaj(dodatni_sadrzaj_id)
+);
+
+CREATE TABLE system.kvart
+(
+  kvart_id INT NOT NULL,
+  ime VARCHAR(50) NOT NULL,
+  grad_id INT NOT NULL,
+  PRIMARY KEY (kvart_id),
+  FOREIGN KEY (grad_id) REFERENCES system.grad(grad_id)
+);
+
+CREATE TABLE system.teren
+(
+  teren_id INT NOT NULL,
+  vrsta_terena VARCHAR(50) NOT NULL,
+  natkriven VARCHAR(50) NOT NULL,
+  ime VARCHAR(50) NOT NULL,
+  sportski_objekt_id INT NOT NULL,
+  kvart_id INT NOT NULL,
+  PRIMARY KEY (teren_id),
+  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
+  FOREIGN KEY (kvart_id) REFERENCES system.kvart(kvart_id)
+);
+
+CREATE TABLE system.rezervacija
+(
+  pocetak VARCHAR(50) NOT NULL,
+  kraj VARCHAR(50) NOT NULL,
+  rezervacija_id INT NOT NULL,
+  teren_id INT NOT NULL,
+  oib INT NOT NULL,
+  PRIMARY KEY (rezervacija_id),
+  FOREIGN KEY (teren_id) REFERENCES system.teren(teren_id),
+  FOREIGN KEY (oib) REFERENCES system.korisnik(oib)
+);
+
+CREATE TABLE system.rekvizit
+(
+  rekvizit_id INT NOT NULL,
+  tip VARCHAR(50) NOT NULL,
+  iznajmljen VARCHAR(50) NOT NULL,
+  sportski_objekt_id INT NOT NULL,
+  rezervacija_id INT NOT NULL,
+  PRIMARY KEY (rekvizit_id),
+  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
+  FOREIGN KEY (rezervacija_id) REFERENCES system.rezervacija(rezervacija_id)
 );
