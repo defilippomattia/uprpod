e79b5fdbd7368fb62e07f50652a542bc4999278d
defilippomattia <defilippomattia@gmail.com>
2022-11-09 16:22:31 +0100

    sql looks good

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ git clone https://github.com/defilippomattia/uprpod
 cd uprpod docker-compose up -d --force-recreate
 login to sqldeveloper
 execute: alter session set nls_date_format = 'dd/MON/yyyy hh24:mi'
+
+### Odgovori
+
+3. 
+Želim dobiti popis svih rezervacija za određeni kvart.
+Primjer:
+Sportski objekt, Sport, Oznaka terena, Datum i vrijeme početka, Datum i vrijeme
+završetka
+TC Maksimir, tenis, 4, 5. 11. 2022. u 10 sati, 5. 11. 2022. u 12 sati
+ŠRC Svetice, nogomet, 2, 6. 11. 2022. u 20 sati, 6. 11. 2022. u 21.30 sati 
+
--- oldsql/sql-create-1.sql
+++ oldsql/sql-create-1.sql
@@ -1,4 +1,4 @@
-CREATE TABLE system.grad
+CREATE TABLE grad
 (
   grad_id INT NOT NULL,
   ime VARCHAR(60) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE system.grad
   PRIMARY KEY (grad_id)
 );
 
-CREATE TABLE system.korisnik
+CREATE TABLE korisnik
 (
   oib VARCHAR(11) NOT NULL,
   ime VARCHAR(60) NOT NULL,
@@ -14,33 +14,33 @@ CREATE TABLE system.korisnik
   PRIMARY KEY (oib)
 );
 
-CREATE TABLE system.dodatni_sadrzaj
+CREATE TABLE dodatni_sadrzaj
 (
   dodatni_sadrzaj_id INT NOT NULL,
   tip VARCHAR(60) NOT NULL,
   PRIMARY KEY (dodatni_sadrzaj_id)
 );
 
-CREATE TABLE system.kvart
+CREATE TABLE kvart
 (
   kvart_id INT NOT NULL,
   ime VARCHAR(60) NOT NULL,
   grad_id INT NOT NULL,
   PRIMARY KEY (kvart_id),
-  FOREIGN KEY (grad_id) REFERENCES system.grad(grad_id)
+  FOREIGN KEY (grad_id) REFERENCES grad(grad_id)
 );
 
-CREATE TABLE system.sportski_objekt
+CREATE TABLE sportski_objekt
 (
   sportski_objekt_id INT NOT NULL,
   ime VARCHAR(60) NOT NULL,
   web_stranica VARCHAR(60) NOT NULL,
   kvart_id INT NOT NULL,
   PRIMARY KEY (sportski_objekt_id),
-  FOREIGN KEY (kvart_id) REFERENCES system.kvart(kvart_id)
+  FOREIGN KEY (kvart_id) REFERENCES kvart(kvart_id)
 );
 
-CREATE TABLE system.teren
+CREATE TABLE teren
 (
   teren_id INT NOT NULL,
   vrsta_terena VARCHAR(60) NOT NULL,
@@ -48,10 +48,10 @@ CREATE TABLE system.teren
   ime VARCHAR(60) NOT NULL,
   sportski_objekt_id INT NOT NULL,
   PRIMARY KEY (teren_id),
-  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id)
+  FOREIGN KEY (sportski_objekt_id) REFERENCES sportski_objekt(sportski_objekt_id)
 );
 
-CREATE TABLE system.rezervacija
+CREATE TABLE rezervacija
 (
   pocetak DATE NOT NULL,
   kraj DATE NOT NULL,
@@ -59,34 +59,34 @@ CREATE TABLE system.rezervacija
   teren_id INT NOT NULL,
   oib VARCHAR(11) NOT NULL,
   PRIMARY KEY (rezervacija_id),
-  FOREIGN KEY (teren_id) REFERENCES system.teren(teren_id),
-  FOREIGN KEY (oib) REFERENCES system.korisnik(oib)
+  FOREIGN KEY (teren_id) REFERENCES teren(teren_id),
+  FOREIGN KEY (oib) REFERENCES korisnik(oib)
 );
 
-CREATE TABLE system.rekvizit
+CREATE TABLE rekvizit
 (
   rekvizit_id INT NOT NULL,
   tip VARCHAR(60) NOT NULL,
   marka VARCHAR(60) NOT NULL,
   sportski_objekt_id INT NOT NULL,
   PRIMARY KEY (rekvizit_id),
-  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id)
+  FOREIGN KEY (sportski_objekt_id) REFERENCES sportski_objekt(sportski_objekt_id)
 );
 
-CREATE TABLE system.rezervacija_rekvizit
+CREATE TABLE iznajmljuje
 (
   rezervacija_id INT NOT NULL,
   rekvizit_id INT NOT NULL,
   PRIMARY KEY (rezervacija_id, rekvizit_id),
-  FOREIGN KEY (rezervacija_id) REFERENCES system.rezervacija(rezervacija_id),
-  FOREIGN KEY (rekvizit_id) REFERENCES system.rekvizit(rekvizit_id)
+  FOREIGN KEY (rezervacija_id) REFERENCES rezervacija(rezervacija_id),
+  FOREIGN KEY (rekvizit_id) REFERENCES rekvizit(rekvizit_id)
 );
 
-CREATE TABLE system.objekt_sadrzaj
+CREATE TABLE sadrzi
 (
   sportski_objekt_id INT NOT NULL,
   dodatni_sadrzaj_id INT NOT NULL,
   PRIMARY KEY (sportski_objekt_id, dodatni_sadrzaj_id),
-  FOREIGN KEY (sportski_objekt_id) REFERENCES system.sportski_objekt(sportski_objekt_id),
-  FOREIGN KEY (dodatni_sadrzaj_id) REFERENCES system.dodatni_sadrzaj(dodatni_sadrzaj_id)
+  FOREIGN KEY (sportski_objekt_id) REFERENCES sportski_objekt(sportski_objekt_id),
+  FOREIGN KEY (dodatni_sadrzaj_id) REFERENCES dodatni_sadrzaj(dodatni_sadrzaj_id)
 );
