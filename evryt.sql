334eb6b9442eb05e3b8d7f233d7e8bb78f553b61
defilippomattia <defilippomattia@gmail.com>
2022-11-10 23:23:45 +0100

    more data

--- evryt.sql
+++ evryt.sql
@@ -39,3 +39,268 @@ SELECT * FROM REZERVACIJA
 SELECT CURRENT_DATE, rezervacija.pocetak from rezervacija
 WHERE CURRENT_DATE < rezervacija.pocetak;
 
+
+
+SELECT SPORTSKI_OBJEKT.IME SPORTSKI_OBJEKT_IME,  TEREN.VRSTA_TERENA, TEREN.TEREN_ID, REZERVACIJA.POCETAK,REZERVACIJA.KRAJ FROM REZERVACIJA
+JOIN KORISNIK ON REZERVACIJA.OIB = KORISNIK.OIB
+JOIN TEREN ON REZERVACIJA.TEREN_ID = TEREN.TEREN_ID
+JOIN SPORTSKI_OBJEKT ON teren.sportski_objekt_id = sportski_objekt.sportski_objekt_id
+WHERE REZERVACIJA.POCETAK BETWEEN 
+    TO_DATE('09-NOV-22 14:00', 'dd-MON-yy hh24:mi') AND
+    TO_DATE('24-NOV-22 14:00', 'dd-MON-yy hh24:mi')
+AND KORISNIK.OIB IN ('0541');
+
+SELECT * FROM REZERVACIJA WHERE OIB IN ('0541')
+
+SELECT * FROM REZERVACIJA
+
+alter session set nls_date_format = 'dd-MON-yy hh24:mi'
+
+SELECT * FROM TEREN
+
+SELECT COUNT(*) FROM TEREN
+
+select round(DBMS_RANDOM.VALUE (1, 5));
+
+select round(dbms_random.value(1,SELECT COUNT(*) system.FROM TEREN)) num from dual;
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    rand_teren := dbms_random.value(1,ukupno_terena);
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        '6',
+        TO_DATE('27-NOV-22 11:00', 'dd-MON-yy hh24:mi'),
+        TO_DATE('27-NOV-22 12:00', 'dd-MON-yy hh24:mi'),
+        rand_teren,
+        '0515'
+    );
+end; 
+/
+
+SELECT * FROM REZERVACIJA 
+
+INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+    '2',
+    TO_DATE('23-DEC-21 14:00', 'dd-MON-yy hh24:mi'),
+    TO_DATE('23-DEC-21 15:00', 'dd-MON-yy hh24:mi'),
+    '1',
+    '0311');
+
+
+    
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        TO_DATE('27-NOV-22 11:00', 'dd-MON-yy hh24:mi'),
+        TO_DATE('27-NOV-22 12:00', 'dd-MON-yy hh24:mi'),
+        rand_teren,
+        '0515'
+    );
+end; 
+/
+
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        TO_DATE('27-NOV-22 11:00', 'dd-MON-yy hh24:mi'),
+        TO_DATE('27-NOV-22 12:00', 'dd-MON-yy hh24:mi'),
+        rand_teren,
+        '0311'
+    );
+end; 
+/
+
+CREATE OR REPLACE FUNCTION simple_function
+    RETURN VARCHAR2
+IS
+BEGIN
+    RETURN '0311';
+END simple_function;
+SELECT * FROM REZERVACIJA;
+SELECT simple_function() FROM dual;
+
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+    rand_start_time date;
+    rand_end_time date;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+    rand_start_time:= TO_DATE(get_rand_start_time(),'dd-MON-yy hh24:mi');
+    rand_end_time:= TO_DATE(rand_start_time+1/24,'dd-MON-yy hh24:mi');
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        rand_start_time,
+        rand_end_time,
+        rand_teren,
+        '0311'
+    );
+end; 
+/
+
+SELECT * FROM REZERVACIJA
+
+
+
+
+CREATE OR REPLACE FUNCTION get_rand_start_time
+    RETURN VARCHAR2
+IS
+    rand_start_time integer;
+    rand_end_time integer;
+    date_only varchar2(30);
+    full_start_date varchar2(30);
+    full_end_date varchar2(30);
+BEGIN
+    rand_start_time := dbms_random.value(10,20);
+    rand_end_time := rand_start_time + 1;
+    date_only:= SUBSTR(CURRENT_DATE,0,9);
+    date_only:= CONCAT(date_only,' ');
+    full_start_date:= CONCAT(date_only,rand_start_time);
+    full_start_date:= CONCAT(full_start_date,':00');
+    RETURN full_start_date;
+END get_rand_start_time;
+
+SELECT get_rand_start_time() FROM dual;
+
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+    rand_start_time date;
+    rand_end_time date;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+    rand_start_time:= TO_DATE(get_rand_start_time(),'dd-MON-yy hh24:mi');
+    rand_end_time:= TO_DATE(rand_start_time+1/24,'dd-MON-yy hh24:mi');
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        rand_start_time,
+        rand_end_time,
+        rand_teren,
+        '0311'
+    );
+end; 
+/
+
+
+
+CREATE OR REPLACE FUNCTION get_rand_start_time
+    RETURN VARCHAR2
+IS
+    rand_start_time integer;
+    rand_end_time integer;
+    date_only varchar2(30);
+    full_start_date varchar2(30);
+    full_end_date varchar2(30);
+BEGIN
+    rand_start_time := dbms_random.value(10,20);
+    rand_end_time := rand_start_time + 1;
+    date_only:= SUBSTR(CURRENT_DATE,0,9);
+    date_only:= CONCAT(date_only,' ');
+    full_start_date:= CONCAT(date_only,rand_start_time);
+    full_start_date:= CONCAT(full_start_date,':00');
+    RETURN full_start_date;
+END get_rand_start_time;
+
+SELECT get_rand_start_time() FROM dual;
+
+SELECT * FROM REZERVACIJA
+
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+    rand_start_time date;
+    rand_end_time date;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+    rand_start_time:= TO_DATE(get_rand_start_time(),'dd-MON-yy hh24:mi');
+    rand_end_time:= TO_DATE(rand_start_time+1/24,'dd-MON-yy hh24:mi');
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        rand_start_time,
+        rand_end_time,
+        rand_teren,
+        '0311'
+    );
+end; 
+/
+
+
+
+CREATE OR REPLACE FUNCTION get_rand_start_time
+    RETURN VARCHAR2
+IS
+    rand_start_time integer;
+    rand_end_time integer;
+    date_only varchar2(30);
+    full_start_date varchar2(30);
+    full_end_date varchar2(30);
+BEGIN
+    rand_start_time := dbms_random.value(10,20);
+    rand_end_time := rand_start_time + 1;
+    date_only:= SUBSTR(CURRENT_DATE,0,9);
+    date_only:= CONCAT(date_only,' ');
+    full_start_date:= CONCAT(date_only,rand_start_time);
+    full_start_date:= CONCAT(full_start_date,':00');
+    RETURN full_start_date;
+END get_rand_start_time;
+
+SELECT get_rand_start_time() FROM dual;
+
+SELECT * FROM REZERVACIJA
--- triggers.sql
+++ triggers.sql
@@ -19,3 +19,49 @@ EXCEPTION
 END;
 /
 
+
+CREATE OR REPLACE FUNCTION get_rand_start_time
+    RETURN VARCHAR2
+IS
+    rand_start_time integer;
+    rand_end_time integer;
+    date_only varchar2(30);
+    full_start_date varchar2(30);
+    full_end_date varchar2(30);
+BEGIN
+    rand_start_time := dbms_random.value(10,20);
+    rand_end_time := rand_start_time + 1;
+    date_only:= SUBSTR(CURRENT_DATE,0,9);
+    date_only:= CONCAT(date_only,' ');
+    full_start_date:= CONCAT(date_only,rand_start_time);
+    full_start_date:= CONCAT(full_start_date,':00');
+    RETURN full_start_date;
+END get_rand_start_time;
+
+declare
+    ukupno_terena integer;
+    rand_teren integer;
+    res_id integer;
+    rand_start_time date;
+    rand_end_time date;
+begin
+    SELECT COUNT(*) INTO ukupno_terena
+    FROM teren;
+    
+    SELECT COUNT(*) INTO res_id
+    FROM rezervacija;
+    
+    rand_teren := dbms_random.value(1,ukupno_terena);
+    rand_start_time:= TO_DATE(get_rand_start_time(),'dd-MON-yy hh24:mi');
+    rand_end_time:= TO_DATE(rand_start_time+1/24,'dd-MON-yy hh24:mi');
+
+    INSERT INTO system.REZERVACIJA(REZERVACIJA_ID,POCETAK,KRAJ,TEREN_ID,OIB) VALUES (
+        res_id+1,
+        rand_start_time,
+        rand_end_time,
+        rand_teren,
+        '0311'
+    );
+end; 
+/
+
